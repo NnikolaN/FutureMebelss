84e36e4d6e3f54d79b5a5bc316c55fec0619f023
Никола Николов <nn18153227@edu.mon.bg>
2023-02-20 20:37:01 +0200

    kolio

---
+++ FutureMebels/Data/Articul.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FutureMebels.Data
+{
+    public class Articul
+    {
+        public int Id { get; set; }
+
+        public int TypeId { get; set; }
+
+        public Type Types { get; set; }
+
+        public  Order Orders { get; set; }
+
+        public Customer Customers { get; set; }
+
+        public string NameModel{ get; set; }
+        
+        public string Category { get; set; }
+
+        public  decimal Size { get; set; }
+
+        public string Applied { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime RegisterOn { get; set; }
+
+        public string Description{ get; set; }
+
+
+    }
+}
---
+++ FutureMebels/Data/Order.cs
@@ -0,0 +1,20 @@
+﻿using FutureMebels.Data;
+using System;
+using System.Collections.Generic;
+
+public class Order
+{
+    public int Id { get; set; }
+
+    public int ArticulId { get; set; }
+
+    public ICollection<Articul>Articuls{ get; set; }
+
+    public int CustomerId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public DateTime RegisterOn{ get; set; }
+
+
+}
